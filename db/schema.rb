cba73d4a166e9144a55a31876c2cd799b432412b
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-12-09 15:57:58 +0000

    Add arask gem to schedule rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_144203) do
+ActiveRecord::Schema.define(version: 2020_12_09_150728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "postgis"
+
+  create_table "arask_jobs", force: :cascade do |t|
+    t.string "job"
+    t.datetime "execute_at"
+    t.string "interval"
+    t.index ["execute_at"], name: "index_arask_jobs_on_execute_at"
+  end
 
   create_table "client_calls", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "access_token"
